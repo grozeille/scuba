42fd21de75a9adb56031e05c51625023a209e720
Mathias Kluba <mathias.kluba@gmail.com>
2017-09-23 17:01:03 +0200

    be able to search using solr be able to collapse tables on wrangling dataset

--- datalake-toolbox-web/src/app/controllers/wranglingDataSetTableSelection/wranglingDataSetTableSelection.controller.js
+++ datalake-toolbox-web/src/app/controllers/wranglingDataSetTableSelection/wranglingDataSetTableSelection.controller.js
@@ -10,21 +10,38 @@ module.exports = {
 function WranglingDataSetTableSelectionController($timeout, $log, $location, $filter, $state, wranglingDataSetService, dataSetService) {
   var vm = this;
   vm.sourceFilter = '';
+  vm.dataSetList = [];
+  vm.pageResult = {
+    totalElements: 0,
+    totalPages: 0,
+    last: true,
+    first: true
+  };
+  vm.currentPage = 1;
+  vm.itemsPerPage = 12;
 
   vm.selectTable = function(database, table) {
-    var selectedTable = $filter('filter')(vm.tables, {database: database, table: table})[0];
+    var selectedTable = $filter('filter')(vm.dataSetList, {database: database, table: table})[0];
 
     wranglingDataSetService.addTable(selectedTable);
     $state.go('wranglingDataSet');
   };
 
-  vm.tables = [];
-
-  activate();
+  vm.loadAllDataSet = function() {
+    dataSetService.getAllDataSet(vm.sourceFilter, vm.currentPage - 1, vm.itemsPerPage).then(function(data) {
+      vm.dataSetList = data.content;
+      vm.pageResult = {
+        totalElements: data.totalElements,
+        last: data.last,
+        first: data.first,
+        totalPages: data.totalPages
+      };
+    });
+  };
 
   function activate() {
-    dataSetService.getAllDataSet().then(function(data) {
-      vm.tables = data.content;
-    });
+    vm.loadAllDataSet();
   }
+
+  activate();
 }
